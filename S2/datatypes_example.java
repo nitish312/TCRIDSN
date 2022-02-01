ce096f4ab2e3dc07508dd7efe45a85818083e7d4
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-02-01 19:06:55 +0530

    s2

---
+++ S2/datatypes_example.java
@@ -0,0 +1,22 @@
+// different data types with variables example
+
+public class datatypes_example {
+    public static void main(String[] args) {
+        byte age= 25;
+        int account_no= 5000001;
+        short rollno =1000 ;
+        long aadhar_number= 1256487845;
+        float price= 10.99f;
+        double d1 = 45678979.50;
+        boolean isEligible=false ;
+        char ch = 'A';
+        String name = "Rajesh";
+
+        System.out.println(age);
+        System.out.println(account_no);
+        System.out.println(rollno);
+        System.out.println(ch);
+        System.out.println(isEligible);
+        //System.out.println(name);
+    }
+}
---
+++ S2/first.java
@@ -0,0 +1,11 @@
+// first program
+public class first {
+    public static void main(String[] args){
+        System.out.println("Hello , welcome to java");
+    }
+}
+
+//in java, every program must contain a class
+//the filename and name of the class should be the same
+//entry point main() : system function
+//to display the output on the screen
