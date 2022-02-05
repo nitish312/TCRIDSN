6448a269f4a96b0460f32b392f0229023e26fb57
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-02-05 09:33:41 +0530

    s5

---
+++ S5/conditional_operator.java
@@ -0,0 +1,15 @@
+//conditional operator
+public class conditional_operator {
+    public static void main(String[] args) {
+        int num1, num2 ;
+        num1= 10 ;
+        num2 = (num1==5)?40:80;
+        System.out.println("value of num2 : " + num2);
+    }
+}
+/*
+if(num1==5)
+    num2=40;
+else
+    num2=80;
+*/
