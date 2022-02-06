3bb5380a5eeb8b9752a867701fdaeb09cebef0fc
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-02-06 05:55:50 +0530

    s5

---
+++ S5/if1.java
@@ -0,0 +1,9 @@
+public class if1 {
+    public static void main(String[] args) {
+        int marks =80 ;
+
+        if(marks > 70){
+            System.out.println("first division");
+        }
+    }
+}
