cb25e309716c1af94a0a4e6d4c978d3960ce6601
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-02-07 16:56:24 +0530

    s6

---
+++ S6/dowhile1.java
@@ -0,0 +1,15 @@
+/*
+do while loop
+exit controlled loop
+at least once loop is going be executed.
+ */
+public class dowhile1 {
+    public static void main(String[] args) {
+        int num =100;
+
+        do {
+            System.out.println(num);
+            num++ ;
+        }while(num<=5);
+    }
+}
