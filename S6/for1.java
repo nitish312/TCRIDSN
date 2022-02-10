58f27ee1a8eb4866db3aa075d14b7042f6946f80
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-02-10 07:32:02 +0530

    s6

---
+++ S6/for1.java
@@ -0,0 +1,10 @@
+// for loop
+
+public class for1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("number is = "+ i);
+        }
+    }
+}
