1911e74b4a9ec878c6190d8da7a6ddc10c0fca6a
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-03-20 18:15:05 +0530

    s13

---
+++ S13/toLowerCase_example.java
@@ -0,0 +1,10 @@
+
+public class toLowerCase_example {
+    public static void main(String[] args) {
+        String s2 = "java";
+        String s3 = "PROGRAMMING";
+
+        System.out.println(s2.toUpperCase());
+        System.out.println(s3.toLowerCase());
+    }
+}
