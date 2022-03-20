5271c037edc6433dbff9b6de8edfef7d17bd06bb
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-03-20 18:15:24 +0530

    s13

---
+++ S13/trim_example.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+//trim()
+public class trim_example {
+    public static void main(String[] args) {
+        String s1 = "    hello      " ;
+        System.out.println(s1.trim() + " how are you?");
+
+
+
+
+
+    }
+}
