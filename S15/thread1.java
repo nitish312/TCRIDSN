326e92c9f078e34c49f7f8ef2c8c43be97f471bb
Nitish Chintakindi <nitishchintakindi@gmail.com>
2022-03-28 16:58:18 +0530

    s15

---
+++ S15/thread1.java
@@ -0,0 +1,13 @@
+//1st way to create thread
+public class thread1 extends Thread {
+    public void run(){
+        System.out.println("thread is running");
+    }
+
+    public static void main(String[] args) {
+        thread1 t1 = new thread1();
+        thread1 t2 = new thread1();
+        t1.start();
+        t2.start();
+    }
+}
